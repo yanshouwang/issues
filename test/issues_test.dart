67d48b77a6735e965f71eaea00a40c1ebc92a7be
yanshouwang <yanshouwang@outlook.com>
2020-12-28 19:49:24 +0800

    simplify code

--- test/issues_test.dart
+++ test/issues_test.dart
@@ -6,32 +6,18 @@ void main() {
   test('#1 Can catch future error', () async {
     final completer = Completer();
     final future = completer.future;
-    final caller = (data) {
-      final error = Exception('ABC');
-      completer.completeError(error);
-    };
-    caller('data');
-    try {
-      await future;
-    } catch (e) {
-      expect('$e', 'Exception: ABC');
-    }
+    final error = Exception('ABC');
+    completer.completeError(error);
+    expect(future, throwsException);
   });
 
   test('#2 Can catch future error', () async {
     final completer = Completer();
     final future = completer.future;
-    final caller = (data) {
-      final error = Exception('ABC');
-      completer.completeError(error);
-    };
-    caller('data');
+    final error = Exception('ABC');
+    completer.completeError(error);
     final duration = Duration(milliseconds: 500);
     await Future.delayed(duration);
-    try {
-      await future;
-    } catch (e) {
-      expect('$e', 'Exception: ABC');
-    }
+    expect(future, throwsException);
   });
 }
